0899ed3471e25f963d432e989fa39640a5b309b4
rude8032 <134621617+MrSingh2996@users.noreply.github.com>
2024-01-04 14:18:59 +0530

    c16

--- Project/core/main/urls.py
+++ Project/core/main/urls.py
@@ -3,6 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('services/', views.services, name='services'),
+    path('about/', views.about, name='about'),
     # path('profile/', views.profile, name='profile'),
     # path('calculator/', views.calculator, name='calculator'),
     # Add other paths for your app views
--- Project/core/main/views.py
+++ Project/core/main/views.py
@@ -4,3 +4,11 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'index.html')
+
+def services(request):
+    return render(request, 'services.html')
+
+
+
+def about(request):
+    return render(request, 'about.html')
