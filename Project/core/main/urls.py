34cb1722c28a2b17368153f88eee92f6b72fd411
rude8032 <134621617+MrSingh2996@users.noreply.github.com>
2024-01-04 18:21:04 +0530

    nn

--- Project/core/main/urls.py
+++ Project/core/main/urls.py
@@ -5,6 +5,11 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('services/', views.services, name='services'),
     path('about/', views.about, name='about'),
+    path('contact/', views.contact, name='contact'),
+    path('blog/', views.blog, name='blog'),
+    path('blog_details/', views.blog_details, name='blog_details'),
+    path('login/', views.login, name='login'),
+    path('register/', views.register, name='register'),
     # path('profile/', views.profile, name='profile'),
     # path('calculator/', views.calculator, name='calculator'),
     # Add other paths for your app views
--- Project/core/main/views.py
+++ Project/core/main/views.py
@@ -9,6 +9,21 @@ def services(request):
     return render(request, 'services.html')
 
 
-
 def about(request):
     return render(request, 'about.html')
+
+
+def contact(request):
+    return render(request, 'contact.html')
+
+def blog(request):
+    return render(request, 'blog.html')
+
+def blog_details(request):
+    return render(request, 'blog_details.html')
+
+def login(request):
+    return render(request, 'login.html')
+
+def register(request):
+    return render(request, 'register.html')
